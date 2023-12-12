8600fc9402fd1888441c2a85a623f3fb3a0262a7
vadim <vadim@XCSENON>
2023-12-12 15:51:55 +0200

    Добавьте файлы проекта.

---
+++ prot/prot.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+class Prototype {
+public:
+    virtual Prototype* clone() const = 0;
+    virtual void display() const = 0;
+};
+
+class ConcretePrototype : public Prototype {
+private:
+    std::string data;
+
+public:
+    ConcretePrototype(const std::string& data) : data(data) {}
+
+    Prototype* clone() const override {
+        return new ConcretePrototype(*this);
+    }
+
+    void display() const override {
+        std::cout << "ConcretePrototype: " << data << std::endl;
+    }
+};
+
+int main() {
+    ConcretePrototype original("Original Data");
+    original.display();
+
+    ConcretePrototype* clone = dynamic_cast<ConcretePrototype*>(original.clone());
+    clone->display();
+
+    delete clone;
+
+    return 0;
+}
